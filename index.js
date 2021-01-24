fd6c9abf17c570a5ae0f684cb24f6ba626a8d18f
Nkechi-Christabel <49942946+Nkechi-Christabel@users.noreply.github.com>
2021-01-24 21:47:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,33 +3,24 @@ const fetch = require('node-fetch')
 const app = express();
 
 
-// localhost:5000/api/rates?base=USD&currency=NGN,EUR,CAD
-// https://api.exchangeratesapi.io/latest?base=CAD
 app.get('/api/rates', (req, res) => {
 
     const {base, currency} = req.query;
     const API = `https://api.exchangeratesapi.io/latest?base=${base}`;
     
     
-
-
-    fetch(API).then(response => response.json())
-    .then(data => {
-    
-      let currencyObj = currency.toUpperCase().split(",").reduce((acc, curr) => (acc[curr] = data.rates[curr], acc), {})
+    fetch(API).then(response => response.json()).then(data => {
+      let currencyObj = currency.toUpperCase().split(",").reduce((acc, curr) => (acc[curr] = data.rates[curr], acc), {});
    
-      
-    console.log(currencyObj)
-        
          res.json(
             {
             "result": {
             "base": `${base}`,
             "dates": `${data.date}`,
             "rates" : currencyObj
-         }
-            }
-         )
+              }
+           }
+        )
     })
     .catch( () => {
           res.status(400).json({msg: "Data not found"}) 
